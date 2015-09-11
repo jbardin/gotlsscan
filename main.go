3a965cca4944bc361156c865c7275e9bc9c25e2b
James Bardin <jbardin@litl.com>
2015-09-11 11:22:43 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+type tlsKV struct {
+	name string
+	val  uint16
+}
+
+var (
+	versions = []tlsKV{
+		{"VersionSSL30", tls.VersionSSL30},
+		{"VersionTLS10", tls.VersionTLS10},
+		{"VersionTLS11", tls.VersionTLS11},
+		{"VersionTLS12", tls.VersionTLS12},
+	}
+
+	ciphers = []tlsKV{
+		{"TLS_RSA_WITH_RC4_128_SHA", tls.TLS_RSA_WITH_RC4_128_SHA},
+		{"TLS_RSA_WITH_3DES_EDE_CBC_SHA", tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA},
+		{"TLS_RSA_WITH_AES_128_CBC_SHA", tls.TLS_RSA_WITH_AES_128_CBC_SHA},
+		{"TLS_RSA_WITH_AES_256_CBC_SHA", tls.TLS_RSA_WITH_AES_256_CBC_SHA},
+		{"TLS_ECDHE_ECDSA_WITH_RC4_128_SHA", tls.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA},
+		{"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA", tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA},
+		{"TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA", tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA},
+		{"TLS_ECDHE_RSA_WITH_RC4_128_SHA", tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA},
+		{"TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA", tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA},
+		{"TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA", tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA},
+		{"TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA", tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA},
+		{"TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256},
+		{"TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256", tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256},
+		{"TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384", tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384},
+		{"TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384", tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384},
+	}
+)
+
+func main() {
+	host := flag.String("host", "", "host")
+	port := flag.String("port", "443", "port")
+	insecure := flag.Bool("insecure", false, "skip certificate verification")
+	flag.Parse()
+
+	if *host == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	cfg := &tls.Config{
+		ServerName:         *host,
+		InsecureSkipVerify: *insecure,
+	}
+
+	for _, v := range versions {
+		fmt.Println("Testing", v.name)
+		any := false
+		for _, c := range ciphers {
+			cfg.MinVersion = v.val
+			cfg.MaxVersion = v.val
+			cfg.CipherSuites = []uint16{c.val}
+
+			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", *host, *port))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			tlsConn := tls.Client(conn, cfg)
+			err = tlsConn.Handshake()
+			if err == nil {
+				any = true
+				fmt.Printf("\t%s\n", c.name)
+			}
+
+			tlsConn.Close()
+		}
+
+		if !any {
+			fmt.Println("\tNOT SUPPORTED")
+		}
+
+	}
+}
